0db42b6accffb548c06e19994745685558f63a22
Akanni Emmanuel <67793955+Harkanni@users.noreply.github.com>
2022-07-04 13:55:35 +0100

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,8 +1,20 @@
+/* 
+	IF YOU EVER WANT TO QUICKLY CHANGE THE FONT ON A WEB PAGE
+	THIS IS HOW YOU DO IT 
+
+	YOU'RE WELCOME :))
+*/
+var body = document.body
+body.style.fontFamily = "Josefin Sans"
+
+//
 let weather = {
 	'apiKey': "25edf8923e4ee5e7762dcd621a01458f",
-	'altKey': "b1b15e88fa797225412429c1c50c122a1",
+	'API_KEY': "a1064ec8f051ec43d04e8d0e055bbec3",
+	'altKey': "b1b15e88fa797225412429c1c50c122a1", //https://api.openweathermap.org/data/2.5/weather?q="cityname"&units=metric&appid="API_KEY"
 	fetchWeather: function(city){
-		fetch("./weatherAPI.js")
+		// fetch("./weatherAPI.js")
+		fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.API_KEY}`)
 		.then((response)=>response.json())
 		.then((data)=> this.displayWeather(data))
 	},
